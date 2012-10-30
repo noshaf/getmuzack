195496b9514c20cd4aca5e785563f9e3e9d22bff
Noah Shafi <noshaf@gmail.com>
2012-10-29 21:12:38 -0700

    finally found password

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 ActionMailer::Base.smtp_settings = {
   :user_name => "app8805336@heroku.com",
+  :password => "pyae83pz",
   :domain => "getmuzack.herokuapp.com",
   :address => "smtp.sendgrid.net",
   :port => 587,
