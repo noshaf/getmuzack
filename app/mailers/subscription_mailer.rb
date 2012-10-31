e407db4959c955913ef0887e546a36675567d228
Noah Shafi <noshaf@gmail.com>
2012-10-30 17:24:30 -0700

    got rake task emails to work

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -6,8 +6,8 @@ class SubscriptionMailer < ActionMailer::Base
     mail(:to => subscription.email, :subject => "Welcome to Get Muzack!")
   end
 
-  def weekly_email
-  	@subscriptions = Subscripions.all
+  def weekly_email(subscriptions)
+  	@subscriptions = subscriptions
   	@subscriptions.each do |s|
   		mail(:to => s.email, :subject => "Get Muzack Weekly Mail")
   	end
--- lib/tasks/send_mail.rake
+++ lib/tasks/send_mail.rake
@@ -1,3 +1,9 @@
-task :send_mail
-	SubscriptionMailer.send_mail.deliver
+task :send_mail => :environment do
+	SubscriptionMailer.weekly_email(Subscription.all).deliver
+end
+
+task :get_emails => :environment do
+	Subscription.all.each do |s|
+		puts s.email
+	end
 end
