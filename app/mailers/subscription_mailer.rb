3e27da8e168aedae5eaafc5ba0d037c00bc8bf17
Noah Shafi <noshaf@gmail.com>
2012-11-01 22:34:40 -0700

    fixed minor name changes

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,15 +1,15 @@
 class SubscriptionMailer < ActionMailer::Base
-  default from: "EDM@GotConcert.com"
+  default from: "GotConcert@ElectricDanceMusic.com"
 
   def welcome_email(subscription)
     @subscription = subscription
-    mail(:to => subscription.email, :subject => "Welcome to Get Muzack!")
+    mail(:to => subscription.email, :subject => "Welcome to Got Concert!")
   end
 
   def weekly_email(subscriptions)
   	@subscriptions = subscriptions
   	@subscriptions.each do |s|
-  		mail(:to => s.email, :subject => "Get Muzack Weekly Mail")
+  		mail(:to => s.email, :subject => "Got Concert Weekly Mail")
   	end
   end
 
